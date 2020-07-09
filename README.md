9035e2031d83275df402f1c4621c803db0386bb6
Dominic Nancekievill <dominic.nancekievill@gresearch.co.uk>
2020-07-09 17:34:08 +0100

    Experimenting with new action

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # request-republish-package
-GitHub Action which exports a package as an artifact and then asks another repo to republish it
+
---
+++ index.js
@@ -0,0 +1,27 @@
+const core = require('@actions/core');
+const {create, UploadOptions} = require('@actions/artifact');
+const github = require('@actions/github');
+const fs = require('fs').promises;
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+(async () => {
+    try {
+        const destOwner = core.getInput('dest-owner');
+        const destRepo = core.getInput('dest-repo');
+        const packagePath = core.getInput('package-path');
+        const repositoryDispatchToken = core.getInput('repository-dispatch-token');
+
+        const octokit = github.getOctokit(pat);
+
+        console.log('Checking SHA256 of ' + packagePath);
+        const {stdout} = await exec('sha256sum ' + packagePath);
+        sha256 = stdout.slice(0, 64);
+        console.log('SHA256 is ' + sha256);
+
+        console.log('Uploading package as a GitHub artifact');
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+})();
